8c20414e7b637208850fa94661676a1eadb0e1b1
Filip Abramowicz <filip.abramowicz@gmail.com>
2024-02-05 19:59:20 +0100

    TestLfsrInitializeSeed added

--- cpp/test/lfsr_test.cpp
+++ cpp/test/lfsr_test.cpp
@@ -9,6 +9,13 @@ TEST(LfsrTests, Test) {
     Lfsr(seed, 16, 14);
 };
 
+TEST(LfsrTests, TestLfsrInitializeSeed) {
+    uint32_t seed = 45;
+    Lfsr myLfsr(seed, 16, 13);
+
+    CHECK_EQUAL(seed, myLfsr.getState());
+};
+
 int main(int argc, char ** argv)
 {
     RUN_ALL_TESTS(argc, argv);
