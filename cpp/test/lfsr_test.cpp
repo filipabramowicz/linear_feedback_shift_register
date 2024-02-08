ea3a6a877caf9fa6d1329b90fe3dda8a6b24ed1a
Filip Abramowicz <filip.abramowicz@gmail.com>
2024-02-08 20:46:05 +0100

    TestLfsrInitializeNokSizeGreaterThanMax added

--- cpp/test/lfsr_test.cpp
+++ cpp/test/lfsr_test.cpp
@@ -12,6 +12,17 @@ TEST(LfsrTests, TestLfsrInitializeSeed) {
     CHECK_EQUAL(seed, myLfsr.getState());
 };
 
+TEST(LfsrTests, TestLfsrInitializeNokSizeGreaterThanMax) {
+    uint32_t seed = 45;
+    Lfsr myLfsr(seed);
+
+    CHECK_EQUAL(seed, myLfsr.getState());
+
+    uint32_t rc = myLfsr.initialize(55, 13);
+
+    CHECK_EQUAL(rc, RC_NOK_SIZE_INT_BITS_GREATER_THAN_32);
+};
+
 int main(int argc, char ** argv)
 {
     RUN_ALL_TESTS(argc, argv);
