b0760fe449aca8e71b532341a205dc7543568b8e
Filip Abramowicz <filip.abramowicz@gmail.com>
2024-02-09 20:49:38 +0100

    TestLfsrInitializeNokTapPositionGreaterThanSize added

--- cpp/test/lfsr_test.cpp
+++ cpp/test/lfsr_test.cpp
@@ -23,6 +23,17 @@ TEST(LfsrTests, TestLfsrInitializeNokSizeGreaterThanMax) {
     CHECK_EQUAL(rc, RC_NOK_SIZE_INT_BITS_GREATER_THAN_32);
 };
 
+TEST(LfsrTests, TestLfsrInitializeNokTapPositionGreaterThanSize) {
+    uint32_t seed = 45;
+    Lfsr myLfsr(seed);
+
+    CHECK_EQUAL(seed, myLfsr.getState());
+
+    uint32_t rc = myLfsr.initialize(16, 18);
+
+    CHECK_EQUAL(rc, RC_NOK_TAP_POSITION_GREATER_THAN_LFSR_SIZE);
+};
+
 int main(int argc, char ** argv)
 {
     RUN_ALL_TESTS(argc, argv);
